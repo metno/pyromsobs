bug fixes, problem with variance not being written to file

OBSstruct.py
@@ -83,6 +83,7 @@ class OBSstruct(object):
                         exec('self.globalatts["%s"] = """%s"""' % (name, getattr(ifile,name)))
                 except:
                     print "No global attributes on file"
+                ifile.close()
                     
             elif isinstance(ifile,OBSstruct):
                 # Create an empty OBSstruct
@@ -231,14 +232,15 @@ class OBSstruct(object):
         if glbattfile:
         
             rem='";,\n'
-            with open(glbattfile) as f:
-                for line in f:
-                    if '=' in line:
-                        name = line.split('=')[0].strip()
-                        cont = ''.join(x for x in line.split('=')[1].split('\n')[0] if x not in rem).strip()
-                    else:                        
-                        cont = ' '.join([cont,''.join(x for x in line if x not in rem).strip()])
-                    self.globalatts[name] = cont.replace('\\n','\n')
+            f = open(glbattfile) 
+            for line in f:
+                if '=' in line:
+                    name = line.split('=')[0].strip()
+                    cont = ''.join(x for x in line.split('=')[1].split('\n')[0] if x not in rem).strip()
+                else:                        
+                    cont = ' '.join([cont,''.join(x for x in line if x not in rem).strip()])
+                self.globalatts[name] = cont.replace('\\n','\n')
+            f.close()
         if not reftime:
             reftime='1970-01-01 00:00:00'
         if not timeunits:
@@ -279,7 +281,7 @@ class OBSstruct(object):
             print "No information on grid type (spherical = 0 ,spherical= 1)"
             print "Consider adding this information!"
         
-        if hasattr(self,'variance') and np.isscalar(self.variance):
+        if hasattr(self,'variance') and np.any(self.variance):
             var=oncid.createVariable('obs_variance','f8',('state_variable',))
             var.long_name = 'global temporal and spatial observation variance'
             var[:]=self.variance[:]

calcFracGrid.py
@@ -93,7 +93,9 @@ def calcFracGrid(S,hisfile,onlyVertical=False,onlyHorizontal=False,multi=False):
         
     # Make sure OBS has Zgrid of same size and type as depth
     OBS.Zgrid=np.ones_like(OBS.depth)
-    grid=SGrid(Dataset(hisfile))
+    fid = Dataset(hisfile)
+    grid=SGrid(fid)
+    fid.close()
     # Find weights to calculate depth at position
     # Must handle points that fall on 
     nipos=np.zeros([4,len(OBS.Xgrid)]); njpos=np.zeros([4,len(OBS.Ygrid)])

extract_modval.py
@@ -5,6 +5,7 @@ from scipy.interpolate import griddata
 def extract_modval(romsfile, Tgrid, Zgrid, Ygrid, Xgrid, otype):
     # remember that Zgrid is one-based, this is not the case for python. 
     # Subtract 1 from Zgrid to get correct layer
+    fid  = Dataset(romsfile)
     Zgrid = Zgrid - 1.
     if Zgrid < 0:
         Zgrid = np.array(0)
@@ -27,7 +28,7 @@ def extract_modval(romsfile, Tgrid, Zgrid, Ygrid, Xgrid, otype):
 
     if otype in [1,2,3]:
     
-        var=Dataset(romsfile).variables[varnames[otype]][tind,yind,xind]
+        var=fid.variables[varnames[otype]][tind,yind,xind]
     
         # set up grids for griddata interpolation
         if len(tind) > 1 :
@@ -74,7 +75,7 @@ def extract_modval(romsfile, Tgrid, Zgrid, Ygrid, Xgrid, otype):
         else:
             zind = [np.floor(Zgrid).astype(int), np.ceil(Zgrid).astype(int)]
         
-        var=Dataset(romsfile).variables[varnames[otype]][tind,zind,yind,xind].squeeze()
+        var=fid.variables[varnames[otype]][tind,zind,yind,xind].squeeze()
 
         if len(tind) > 1 :
             t = np.zeros_like(var)
@@ -152,7 +153,7 @@ def extract_modval(romsfile, Tgrid, Zgrid, Ygrid, Xgrid, otype):
                     second[a,b] = ind2[b]
             return griddata((first.flatten(),second.flatten()),var.flatten(), (grid1,grid2))
         
-        
+        fid.close()
         if len(var.shape) == 1 and var.shape[0] > 1 :
             # Determine which dimensions to use, set pointers to appropriate variables
             if 't' in locals():

roms2obs.py
@@ -12,7 +12,6 @@ On output
 MOD - a OBSstruct object similar to OBS, but with model values in MOD.values 
 
 '''
-
 from netCDF4 import Dataset
 from .OBSstruct import OBSstruct
 from scipy.interpolate import griddata
@@ -33,24 +32,24 @@ def roms2obs(S, romsfile):
        OBS = OBSstruct(fid)
     else:
        OBS = OBSstruct(S)
-    OBS.toarray()
+    fid = Dataset(romsfile)
     try:    
-        time = Dataset(romsfile).variables['ocean_time'][:]/86400.
+        time = fid.variables['ocean_time'][:]/86400.
     except:
         try:
-            time = Dataset(romsfile).variables['time'][:]/86400.
+            time = fid.variables['time'][:]/86400.
         except:
             try:
-                time = Dataset(romsfile).variables['clim_time']
+                time = fid.variables['clim_time']
                 if "seconds" in time.units:
-                    time = Dataset(romsfile).variables['clim_time'][:]/86400.
+                    time = fid.variables['clim_time'][:]/86400.
                 else:
                     # Assume days:
-                    time = Dataset(romsfile).variables['clim_time'][:]
+                    time = fid.variables['clim_time'][:]
             except:
                 print 'Not able to find time variable, exiting'
                 exit()
-    
+    fid.close()
 
     # First calculate fractional time Index. Do this for every survey time
     # If more than one day (24h) from obstime to model time, set Tgrid to None
@@ -95,8 +94,8 @@ def roms2obs(S, romsfile):
         for o in range(0,O.Ndatum):
             arguments.append([romsfile, Tgrid[n],O.Zgrid[o],O.Ygrid[o],O.Xgrid[o],O.type[o]])
             indices.append([index[o],Terror[n]])
-    pool=Pool()
-    #allresults = map(multi_run_wrapper,arguments)
+    pool=Pool(6)
+    allresults = pool.map(multi_run_wrapper,arguments)
     
     print 'Length of results from multi_run_wrapper: ', len(allresults),  len(indices)
     print 'Number of observations on OBS object: ', OBS.Ndatum