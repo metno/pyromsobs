01039b0bd72ab371f481abc28edb2c183490443b
Ann Kristin Sperrevik <annks@pc4718.pc.met.no>
2017-05-05 11:18:53 +0200

    # ------------------------ >8 ------------------------ # Do not touch the line above. # Everything below will be removed. # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch master # Your branch is up-to-date with 'origin/master'. # Changes to be committed: # # 	modified:   helpers.py # # Untracked files: # # 	fast_merge.py

--- helpers.py
+++ helpers.py
@@ -20,9 +20,9 @@ def setDimensions(OBS):
         Nobs[item[0]] = len(np.array(np.argwhere(OBS.time==item[1])))
     OBS.Nobs=Nobs
     return OBS
-    
+
 def accum_np(accmap, a, func=np.sum):
-    indices = np.where(np.ediff1d(accmap, to_begin=[1],to_end=[1]))[0]     
+    indices = np.where(np.ediff1d(accmap, to_begin=[1],to_end=[1]))[0]
     vals = np.zeros(len(indices) - 1)
     for i in xrange(len(indices) - 1):
         vals[i] = func(a[indices[i]:indices[i+1]])
@@ -30,27 +30,27 @@ def accum_np(accmap, a, func=np.sum):
 
 def sort_ascending(OBS):
     '''
-    Takes an observation structure and sorts the observations 
+    Takes an observation structure and sorts the observations
     according to ascending time'
     '''
     field_list=OBS.getfieldlist()
     OBS.tolist()
     # order according to ascending obs_time
-        
+
     tmplist = zip(*[getattr(OBS,names) for names in field_list])
     tmplist.sort()
     tmplist = zip(*tmplist)
-    
-    for n in range(0,len(field_list)): 
+
+    for n in range(0,len(field_list)):
         if (len(OBS.__dict__[names])):
             setattr(OBS,field_list[n],tmplist[n])
     OBS.toarray()
     OBS=setDimensions(OBS)
-    
+
     return OBS
 def popEntries(popindex, OBS):
     field_list=OBS.getfieldlist()
-    OBS.tolist()   
+    OBS.tolist()
 
     popindex=np.array(popindex).squeeze()
     if popindex.size>1:
@@ -59,9 +59,18 @@ def popEntries(popindex, OBS):
            for names in field_list:
                 if (len(OBS.__dict__[names])):
                     del OBS.__dict__[names][index]
-    elif (popindex.size<2) & (popindex.size>0): 
+    elif (popindex.size<2) & (popindex.size>0):
        index=popindex
-       for names in field_list:
            if (len(OBS.__dict__[names])):
                del OBS.__dict__[names][index]
     return OBS
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    '''
+    alist.sort(key=natural_keys) sorts in human order
+    http://nedbatchelder.com/blog/200712/human_sorting.html
+    (See Toothy's implementation in the comments)
+    '''
+    return [ atoi(c) for c in re.split('(\d+)', text) ]
