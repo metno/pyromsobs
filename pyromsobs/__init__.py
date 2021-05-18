05db532dba8abf090d498f4536c09488668a2b87
annks <a.k.sperrevik@gmail.com>
2021-05-18 08:53:26 +0200

    new polygon method (shapely)

--- pyromsobs/__init__.py
+++ pyromsobs/__init__.py
@@ -3,7 +3,7 @@ from .get_profiles import get_profiles
 from .adjust_survey import adjust_survey
 from .calcFracGrid import calcFracGrid
 from .merge import merge
-from .obs_ijpos import obs_ijpos
+from .obs_ijpos import obs_ijpos, point_in_polygon
 from .plot_obs import plot_obs
 from .plot_datum import plot_datum
 from .remove_duplicates import remove_duplicates
--- pyromsobs/obs_ijpos.py
+++ pyromsobs/obs_ijpos.py
@@ -4,6 +4,9 @@ import pyproj as proj
 from scipy.interpolate import griddata
 import numpy as np
 import scipy
+import cartopy.crs as ccrs
+from shapely.geometry import Polygon, Point
+
 def obs_ijpos(gridfile,lons,lats,coor):
     """
     Finds fractional gridpoint locations of observation in a ROMS grid
@@ -12,8 +15,14 @@ def obs_ijpos(gridfile,lons,lats,coor):
     gfh= netCDF4.Dataset(gridfile)
     cartesian=0
     if (coor=='r'):
-        latr=gfh.variables['lat_rho'][:,:]
-        lonr=gfh.variables['lon_rho'][:,:]
+        try:
+            
+            latr=gfh.variables['lat_rho'][:,:]
+            lonr=gfh.variables['lon_rho'][:,:]
+        except:
+            latr=gfh.variables['latitude'][:,:]
+            lonr=gfh.variables['longitude'][:,:]
+            
 
         try:
             xr=gfh.variables['xi_rho'][:]
@@ -46,49 +55,11 @@ def obs_ijpos(gridfile,lons,lats,coor):
             xr=gfh.variables['x_v'][:]
             yr=gfh.variables['y_v'][:]
 
-
-    edge=[]
-    first=np.array([(lonr[:-1,0],latr[:-1,0])]).squeeze()
-    second=np.array([(lonr[-1,:-1],latr[-1,:-1])]).squeeze()
-    third=np.fliplr(np.array([(lonr[1:,-1],latr[1:,-1])]).squeeze())
-    fourth=np.fliplr(np.array([(lonr[0,1:],latr[0,1:])]).squeeze())
-
-    N=list(range(0,first.shape[1],10))
-    N.append(first.shape[1]-1)
-    for n in N:
-        edge.append(tuple(first[:,n]))
-
-    N=list(range(0,second.shape[1],10))
-    N.append(second.shape[1]-1)
-
-    for n in N:
-        edge.append(tuple(second[:,n]))
-
-    N=list(range(0,third.shape[1],10))
-    N.append(third.shape[1]-1)
-
-    for n in N:
-        edge.append(tuple(third[:,n]))
-
-    N=list(range(0,fourth.shape[1],10))
-    N.append(fourth.shape[1]-1)
-
-    for n in N:
-        edge.append(tuple(fourth[:,n]))
-
-    poly=edge
-    if (lats.size>1):
-        IN=np.zeros_like(lats)
-        for n in range(0,lats.size):
-            IN[n]=pip(lons[n],lats[n],poly)
-        ind=np.where(IN)[0]
+    IN = point_in_polygon(lonr, latr, lons, lats)
+    ind=np.where(IN)[0]
+    
+    if lats.size >1: 
         lons=lons[ind]; lats=lats[ind]
-    else:
-        IN=pip(lons,lats,poly)
-    #if (np.sum(IN>0):
-    # read the proj4 string from the netcdf file, it is used in all
-    # necessary calculations
-
     try:
         try:
             mapstr=str(gfh.variables['h'].getncattr('mapping'))
@@ -131,7 +102,9 @@ def obs_ijpos(gridfile,lons,lats,coor):
         if ( (dx1 == dx2) & (dx1==dx3) & (dx2==dx3) & (dy1 == dy2) & (dy1==dy3) & (dy2==dy3) ):
             cartesian=1
             gridproj=proj.Proj("+proj=latlong +datum=WGS84")
+   
 
+    
     if hasproj:
         dx=xr[1]-xr[0]
         dy=yr[1]-yr[0]
@@ -157,10 +130,10 @@ def obs_ijpos(gridfile,lons,lats,coor):
         xi=np.zeros_like(lonr); yi=np.zeros([lonr.shape[1],lonr.shape[0]])
         xi[:,:]=x; yi[:,:]=y; yi=np.swapaxes(yi,1,0)
         zi=scipy.interpolate.griddata((lonr.flatten(),latr.flatten()),xi.flatten(),(lons,lats))
-
         ipos=zi
         zi=scipy.interpolate.griddata((lonr.flatten(),latr.flatten()),yi.flatten(),(lons,lats))
         jpos=zi
+        
     if 'ind' in locals():
         oipos=np.ones(IN.shape)*-999.; ojpos=np.ones(IN.shape)*-999.
         oipos[ind]=ipos; ojpos[ind]=jpos
@@ -172,62 +145,50 @@ def obs_ijpos(gridfile,lons,lats,coor):
             ojpos = np.array([-999.])
     gfh.close()
     return oipos,ojpos
-'''
-def pip(x,y,poly):
-    n = len(poly)
-    inside = False
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
-                    if p1y != p2y:
-                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or x <= xints:
-                        inside = not inside
-        p1x,p1y = p2x,p2y
-    return inside
 
-'''
-def pip(x,y,poly):
-    #print len(poly)
-    # check if point is a vertex
-    if (x,y) in poly: return True
+def xy_from_lonlat(proj, lon, lat):
+    try:
+        test = len(lon)
+    except:
+        lon = np.array([lon])
+        lat = np.array([lat])
+    transform = proj.transform_points(ccrs.PlateCarree(), lon, lat)
+    x = transform[..., 0]
+    y = transform[..., 1]
+    return x[0], y[0]
 
-    # check if point is on a boundary
-    for i in range(len(poly)):
-        p1 = None
-        p2 = None
-        if i==0:
-            p1 = poly[0]
-            p2 = poly[-1]
-        else:
-            p1 = poly[i-1]
-            p2 = poly[i]
-        if p1[1] == p2[1] and p1[1] == y and x > min(p1[0], p2[0]) and x < max(p1[0], p2[0]):
-            return True
+def polygon_from_lonlat(lon, lat, proj= ccrs.LambertConformal(central_longitude = 0, central_latitude = 0, false_easting = 0.0, false_northing = 0.0)):
+    coordinates = []
+    geocoordinates = []
+    l, m = lon.shape
+    for i in range(0, l, int(l/10)):
+        j = 0
+        x, y = xy_from_lonlat(proj, lon[i,j], lat[i,j])
+        coordinates.append([x,y])
+        geocoordinates.append([lon[i,j], lat[i,j]])
+    for j in range(0, m, int(m/10)):
+        i = -1
+        x, y = xy_from_lonlat(proj, lon[i,j], lat[i,j])
+        coordinates.append([x,y])
+        geocoordinates.append([lon[i,j], lat[i,j]])
+    for i in range(l-1, 0, -int(l/10)):
+        j = -1
+        if i >= 0:
+            x, y = xy_from_lonlat(proj, lon[i,j], lat[i,j])
+            coordinates.append([x,y])
+            geocoordinates.append([lon[i,j], lat[i,j]])
+    for j in range(m-1, 0-int(m/10), -int(m/10)):
+        i = 0
+        if j >= 0:
+            x, y = xy_from_lonlat(proj, lon[i,j], lat[i,j])
+            coordinates.append([x,y])
+            geocoordinates.append([lon[i,j], lat[i,j]])
+    return Polygon(coordinates), Polygon(geocoordinates)
 
-    n = len(poly)
-    inside = False
-    p1x,p1y = poly[0]
-    for i in range(n+1):
-        p2x,p2y = poly[i % n]
-        if y > min(p1y,p2y):
-            if y <= max(p1y,p2y):
-                if x <= max(p1x,p2x):
+def point_in_polygon(mlon, mlat, olon, olat, proj = ccrs.LambertConformal(central_longitude = 0, central_latitude = 0, false_easting = 0.0, false_northing = 0.0)):
+    poly, geopoly = polygon_from_lonlat(mlon, mlat , proj )
+    IN = np.zeros_like(olat, dtype = object)
 
-
-                    if p1y != p2y:
-                        xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                        if not ( (x - p1x)*(p2y-p1y) >= (y-p1y)*(p2x-p1x)) :
-                            inside=False
-                            break
-
-
-                    if  (p1x == p2x or x < xints):
-                        inside = True
-
-
-        p1x,p1y = p2x,p2y
-    return inside
+    for n in range(0, olat.size):
+        IN[n] = poly.contains(Point(xy_from_lonlat(proj, olon[n], olat[n])))
+    return IN
