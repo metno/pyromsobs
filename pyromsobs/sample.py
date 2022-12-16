c2c622b7d7524b5a37e0027a3436e2d15737f2be
annks <a.k.sperrevik@gmail.com>
2022-12-16 09:49:03 +0000

    updated roppy files to latest version

--- pyromsobs/sample.py
+++ pyromsobs/sample.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
-# This file is copied from https://github.com/bjornaa/roppy
 
 """Utilities functions for sampling output from the ROMS
+
 Horizontal sampling
 -------------------
+
 :func:`sample2D`
   Sample a 2D field given at rho-points
 :func:`sample2DU`
   Sample a 2D field given at u-points
 :func:`sample2DV`
   Sample a 2D field given at v-points
+
 """
 
 # -----------------------------------
@@ -21,7 +23,7 @@ Horizontal sampling
 
 from __future__ import division
 
-import datetime
+# import datetime
 import numpy as np
 
 # ---------------------
@@ -29,14 +31,19 @@ import numpy as np
 
 def sample2D2(F, X, Y):
     """Bilinear sample of a 2D field
+
     *F* : 2D array
+
     *X*, *Y* : position in grid coordinates, scalars or compatible arrays
+
     Note reversed axes, for integers i and j we have
       ``sample2D(F, i, j) = F[j,i]``
+
     Using bilinear interpolation
+
     """
 
-    Z = np.add(X, Y)     # Test for compatibility
+    Z = np.add(X, Y)  # Test for compatibility
     if np.isscalar(Z):  # Both X and Y are scalars
         I = int(X)
         J = int(Y)
@@ -46,45 +53,52 @@ def sample2D2(F, X, Y):
         # Make arrays of common shape
         X0 = X + np.zeros_like(Z)
         Y0 = Y + np.zeros_like(Z)
-        I = X0.astype('int')
-        J = Y0.astype('int')
-        P = X0 - I
-        Q = Y0 - J
+        I = X0.astype("int")
+        J = Y0.astype("int")
+        P = X0 - I  # type: ignore
+        Q = Y0 - J  # type: ignore
 
-    W00 = (1-P)*(1-Q)
-    W01 = (1-P)*Q
-    W10 = P*(1-Q)
-    W11 = P*Q
+    W00 = (1 - P) * (1 - Q)
+    W01 = (1 - P) * Q
+    W10 = P * (1 - Q)
+    W11 = P * Q
 
-    return W00*F[J,I] + W01*F[J+1,I] + W10*F[J,I+1] + W11*F[J+1,I+1]
+    return W00 * F[J, I] + W01 * F[J + 1, I] + W10 * F[J, I + 1] + W11 * F[J + 1, I + 1]
+
 
 # --------------------------------------------------
 
 
 def sample2DU(F, X, Y):
-    return sample2D(F, X-0.5, Y)
+    return sample2D(F, X - 0.5, Y)
+
 
 # --------------------------------------------------
 
 
 def sample2DV(F, X, Y):
-    return sample2D(F, X, Y-0.5)
+    return sample2D(F, X, Y - 0.5)
+
 
 # -------------------------------------------------
 
 
 def sample2D_masked(F, M, X, Y):
     """Bilinar sample of a 2D field
+
     F = 2D array, M = mask (=1 on sea, = 0 on land)
     X, Y = position in grid coordinates, scalars or compatible arrays
+
     Note reversed axes, for integers i and j we have
     sample2D(F, i, j) = F[j,i]
+
     Using linear interpolation
+
     """
 
     masked = True
 
-    Z = np.add(X,Y)     # Test for compatibility
+    Z = np.add(X, Y)  # Test for compatibility
     if np.isscalar(Z):  # Both X and Y are scalars
         I = int(X)
         J = int(Y)
@@ -94,51 +108,64 @@ def sample2D_masked(F, M, X, Y):
         # Make arrays of common shape
         X0 = X + np.zeros_like(Z)
         Y0 = Y + np.zeros_like(Z)
-        I = X0.astype('int')
-        J = Y0.astype('int')
+        I = X0.astype("int")
+        J = Y0.astype("int")
         P = X0 - I
         Q = Y0 - J
 
-    W00 = M[J,I]*(1-P)*(1-Q)
-    W01 = M[J+1,I]*(1-P)*Q
-    W10 = M[J,I+1]*P*(1-Q)
-    W11 = M[J+1,I+1]*P*Q
+    W00 = M[J, I] * (1 - P) * (1 - Q)
+    W01 = M[J + 1, I] * (1 - P) * Q
+    W10 = M[J, I + 1] * P * (1 - Q)
+    W11 = M[J + 1, I + 1] * P * Q
     if masked:
-        W00 = M[J,I]     * W00
-        W01 = M[J+1,I]   * W01
-        W10 = M[J,I+1]   * W10
-        W11 = M[J+1,I+1] * W11
+        W00 = M[J, I] * W00
+        W01 = M[J + 1, I] * W01
+        W10 = M[J, I + 1] * W10
+        W11 = M[J + 1, I + 1] * W11
     SW = W00 + W01 + W10 + W11
-    F_interp = np.where(SW > 0,
-            (W00*F[J,I] + W01*F[J+1,I] + W10*F[J,I+1] + W11*F[J+1,I+1])/SW,
-             0.0)
+    F_interp = np.where(
+        SW > 0,
+        (W00 * F[J, I] + W01 * F[J + 1, I] + W10 * F[J, I + 1] + W11 * F[J + 1, I + 1])
+        / SW,
+        0.0,
+    )
 
     return F_interp
+
 
 # ------------------
 
 
 def sample2D(F, X, Y, mask=None, undef_value=0.0, outside_value=None):
     """Bilinear sample of a 2D field
+
     *F* : 2D array, shape = (jmax, imax)
+
     *X*, *Y* : position in grid coordinates, scalars or compatible arrays
+
     *mask* : if present must be a 2D matrix with 1 at valid
              and zero at non-valid points
+
     *undef_value* : value to put at undefined points
+
     *outside_value* : value to return outside the grid
                 defaults to None,
                 raising ValueError if any points are outside
+
     Note reversed axes, for integers i and j we have
       ``sample2D(F, i, j) = F[j,i]``
+
     If jmax, imax = F.shape
     then inside values requires 0 <= x < imax-1, 0 <= y < jmax-1
+
     Using bilinear interpolation
+
     """
 
     # --- Argument checking ---
 
     # X and Y should be broadcastable to the same shape
-    Z = np.add(X,Y)
+    Z = np.add(X, Y)
     # scalar is True if both X and Y are scalars
     scalar = np.isscalar(Z)
 
@@ -156,12 +183,12 @@ def sample2D(F, X, Y, mask=None, undef_value=0.0, outside_value=None):
     # Find integer I, J such that
     # 0 <= I <= X < I+1 <= imax-1, 0 <= J <= Y < J+1 <= jmax-1
     # and local increments P and Q
-    I = X0.astype('int')
-    J = Y0.astype('int')
+    I = X0.astype("int")
+    J = Y0.astype("int")
     P = X0 - I
     Q = Y0 - J
 
-    outside = (X0 < 0) | (X0 >= imax-1) | (Y0 < 0) | (Y0 >= jmax-1)
+    outside = (X0 < 0) | (X0 >= imax - 1) | (Y0 < 0) | (Y0 >= jmax - 1)
     if np.any(outside):
         if outside_value is None:
             raise ValueError("point outside grid")
@@ -175,43 +202,51 @@ def sample2D(F, X, Y, mask=None, undef_value=0.0, outside_value=None):
         #    J = np.array(0)
 
     # Weights for bilinear interpolation
-    W00 = (1-P)*(1-Q)
-    W01 = (1-P)*Q
-    W10 = P*(1-Q)
-    W11 = P*Q
-    SW = 1.0   # Sum of weigths
+    W00 = (1 - P) * (1 - Q)
+    W01 = (1 - P) * Q
+    W10 = P * (1 - Q)
+    W11 = P * Q
+    SW = 1.0  # Sum of weigths
 
     if mask is not None:
-        W00 = mask[J,I]     * W00
-        W01 = mask[J+1,I]   * W01
-        W10 = mask[J,I+1]   * W10
-        W11 = mask[J+1,I+1] * W11
+        W00 = mask[J, I] * W00
+        W01 = mask[J + 1, I] * W01
+        W10 = mask[J, I + 1] * W10
+        W11 = mask[J + 1, I + 1] * W11
         SW = W00 + W01 + W10 + W11
 
-    SW = np.where(SW==0, -1.0, SW)  # Avoid division by zero below
-    S = np.where(SW <= 0, undef_value,
-          (W00*F[J,I] + W01*F[J+1,I] + W10*F[J,I+1] + W11*F[J+1,I+1])/SW)
+    SW = np.where(SW == 0, -1.0, SW)  # Avoid division by zero below
+    S = np.where(
+        SW <= 0,
+        undef_value,
+        (W00 * F[J, I] + W01 * F[J + 1, I] + W10 * F[J, I + 1] + W11 * F[J + 1, I + 1])
+        / SW,
+    )
 
     # Set in outside_value
     if outside_value:
         S = np.where(outside, outside_value, S)
 
-#   Scalar input gives scalar output
+    #   Scalar input gives scalar output
     if scalar:
         S = float(S)
 
     return S
+
 
 # -----------------------------------------
 
 
 def bilin_inv(f, g, F, G, maxiter=7, tol=1.0e-7):
     """Inverse bilinear interpolation
+
     f, g : scalars or arrays of same shape
     F, G : 2D arrays of the same shape
+
     returns x, y : shaped like f and g
     such that F and G linearly interpolated to x, y
     returns f and g
+
     """
 
     imax, jmax = F.shape
@@ -222,25 +257,24 @@ def bilin_inv(f, g, F, G, maxiter=7, tol=1.0e-7):
 
     if scalar:
         if not np.isscalar(g):
-            raise ValueError(
-                "Target values must both be scalars or both arrays")
+            raise ValueError("Target values must both be scalars or both arrays")
         # initial guess
-        x = 0.5*imax
-        y = 0.5*jmax
+        x = 0.5 * imax
+        y = 0.5 * jmax
 
     else:  # vector target
         f = np.asarray(f)
         g = np.asarray(g)
         fshape = f.shape
         if g.shape != fshape:
-            raise ValueError( "Target arrays must have the same shape")
+            raise ValueError("Target arrays must have the same shape")
         # Make 1D
-        #f = f.ravel()
-        #g = g.ravel()
+        # f = f.ravel()
+        # g = g.ravel()
 
         # initial guess
-        x = np.zeros_like(f) + 0.5*imax
-        y = np.zeros_like(f) + 0.5*jmax
+        x = np.zeros_like(f) + 0.5 * imax
+        y = np.zeros_like(f) + 0.5 * jmax
 
     for t in range(maxiter):
 
@@ -248,34 +282,42 @@ def bilin_inv(f, g, F, G, maxiter=7, tol=1.0e-7):
             i = int(x)
             j = int(y)
         else:
-            i = x.astype('i')
-            j = y.astype('i')
+            i = x.astype("i")
+            j = y.astype("i")
 
         p, q = x - i, y - j
 
         # Bilinear estimate of F[x,y] and G[x,y]
-        Fs = (1-p)*(1-q)*F[i,j] + p*(1-q)*F[i+1,j] + \
-             (1-p)*q*F[i,j+1] + p*q*F[i+1,j+1]
-        Gs = (1-p)*(1-q)*G[i,j] + p*(1-q)*G[i+1,j] + \
-             (1-p)*q*G[i,j+1] + p*q*G[i+1,j+1]
+        Fs = (
+            (1 - p) * (1 - q) * F[i, j]
+            + p * (1 - q) * F[i + 1, j]
+            + (1 - p) * q * F[i, j + 1]
+            + p * q * F[i + 1, j + 1]
+        )
+        Gs = (
+            (1 - p) * (1 - q) * G[i, j]
+            + p * (1 - q) * G[i + 1, j]
+            + (1 - p) * q * G[i, j + 1]
+            + p * q * G[i + 1, j + 1]
+        )
 
-        H = (Fs - f)**2 + (Gs - g)**2
+        H = (Fs - f) ** 2 + (Gs - g) ** 2
 
         if np.all(H < tol):
             break
 
         # Estimate Jacobi matrix
-        Fx = (1-q)*(F[i+1,j]-F[i,j]) + q*(F[i+1,j+1]-F[i,j+1])
-        Fy = (1-p)*(F[i,j+1]-F[i,j]) + p*(F[i+1,j+1]-F[i+1,j])
-        Gx = (1-q)*(G[i+1,j]-G[i,j]) + q*(G[i+1,j+1]-G[i,j+1])
-        Gy = (1-p)*(G[i,j+1]-G[i,j]) + p*(G[i+1,j+1]-G[i+1,j])
+        Fx = (1 - q) * (F[i + 1, j] - F[i, j]) + q * (F[i + 1, j + 1] - F[i, j + 1])
+        Fy = (1 - p) * (F[i, j + 1] - F[i, j]) + p * (F[i + 1, j + 1] - F[i + 1, j])
+        Gx = (1 - q) * (G[i + 1, j] - G[i, j]) + q * (G[i + 1, j + 1] - G[i, j + 1])
+        Gy = (1 - p) * (G[i, j + 1] - G[i, j]) + p * (G[i + 1, j + 1] - G[i + 1, j])
 
         # Newton-Raphson step
         # Jinv = np.linalg.inv([[Fx, Fy], [Gx, Gy]])
         # incr = - np.dot(Jinv, [Fs-f, Gs-g])
         # x = x + incr[0], y = y + incr[1]
-        det = Fx*Gy - Fy*Gx
-        x = x - ( Gy*(Fs-f) - Fy*(Gs-g)) / det
-        y = y - (-Gx*(Fs-f) + Fx*(Gs-g)) / det
+        det = Fx * Gy - Fy * Gx
+        x = x - (Gy * (Fs - f) - Fy * (Gs - g)) / det
+        y = y - (-Gx * (Fs - f) + Fx * (Gs - g)) / det
 
     return x, y
--- pyromsobs/sgrid.py
+++ pyromsobs/sgrid.py
@@ -1,12 +1,15 @@
-# -*- coding: utf-8 -*-
-# This file is copied from https://github.com/bjornaa/roppy
 """
+
+Main SGrid class in roppy
+
 Classes
 -------
+
 :class:`SGrid`
   Simple grid class
 :class:`Section`
   Class for vertical sections
+
 """
 
 # -----------------------------------
@@ -16,21 +19,20 @@ Classes
 # 2010-09-30
 # -----------------------------------
 
-import numpy as np
-from netCDF4 import Dataset
+from typing import Optional, Tuple, Mapping, Any
 
-from .depth import sdepth, zslice, s_stretch
-from .sample import sample2D, bilin_inv
+import numpy as np
+from netCDF4 import Dataset  # type: ignore
+from roppy.depth import sdepth, zslice, s_stretch
+from roppy.sample import sample2D, bilin_inv
 
 # ------------------------------------------------------
 # Classes
 # ------------------------------------------------------
 
-# -------------------------------
 
-
-class _Lazy(object):
-    """ Make lazy properties doing work only when and if needed"""
+class _Lazy:
+    """Make lazy properties doing work only when and if needed"""
 
     # Recipe by Scott David Daniels
     # http://code.activestate.com/recipes/363602-lazy-property-evaluation/
@@ -45,25 +47,29 @@ class _Lazy(object):
         setattr(obj, self._calculate.__name__, value)
         return value
 
+
 # ------------------------------
 
 
 class SGrid(object):
 
     """Simple ROMS grid object
+
     Simple, minimal ROMS 3D grid object, for keeping important
     information together. Meant to be compatible with
     roms2soda's grdClass.
-    Note: Can not (yet) be initialized from a standard grd-file,
-    use initial, history or average file or supply extra vertical
-    information by Vinfo or Vfile.
+
     Typical usage::
-    >>> fid = Dataset(roms_file)
-    >>> grd = SGrid(fid)
+
+    >>> ncid = Dataset(roms_file)
+    >>> grd = SGrid(ncid)
+
     More arguments::
-    >>> fid = Dataset(roms_file)
-    >>> Vinfo = {'N' : 32, 'hc' : 10, 'theta_s' : 0.8, 'theta_b' : 0.4}
-    >>> grd = SGrid(fid, subgrid=(100, 121, 60, 161), Vinfo=Vinfo)
+
+    >>> ncid = Dataset(roms_file)
+    >>> Vinfo = dict(N=32, hc=10, theta_s=0.8, theta_b=0.4, Vtransform=2, Vstretching=4)
+    >>> grd = SGrid(ncid, subgrid=(100, 121, 60, 161), Vinfo=Vinfo)
+
     """
 
     def __init__(self, ncid, subgrid=None, Vinfo=None, Vfile=None):
@@ -71,7 +77,6 @@ class SGrid(object):
         self.ncid = ncid
         self.subgrid = subgrid
         self._Vinfo = Vinfo
-        self._Vfile = Vfile
 
         self._init_horizontal()
         self._init_vertical()
@@ -80,7 +85,7 @@ class SGrid(object):
 
         # (sub-)grid limits
         # i0 <= i < i1, j0 <= j < j1
-        Mp, Lp = self.ncid.variables['h'].shape
+        Mp, Lp = self.ncid.variables["h"].shape
         if self.subgrid:
             i0 = self.subgrid[0]
             i1 = self.subgrid[1]
@@ -111,17 +116,17 @@ class SGrid(object):
             self.j0, self.j1 = 0, Mp
 
         # Shape of the grid
-        self.shape = (self.j1-self.j0, self.i1-self.i0)
+        self.shape = (self.j1 - self.j0, self.i1 - self.i0)
 
         # Slices for rho-points
         self.I = slice(self.i0, self.i1)
         self.J = slice(self.j0, self.j1)
 
         # U and V-points
-        i0_u = max(0, self.i0-1)
-        i1_u = min(self.i1, Lp-1)
-        j0_v = max(0, self.j0-1)
-        j1_v = min(self.j1, Mp-1)
+        i0_u = max(0, self.i0 - 1)
+        i1_u = min(self.i1, Lp - 1)
+        j0_v = max(0, self.j0 - 1)
+        j1_v = min(self.j1, Mp - 1)
         self.i0_u = i0_u
         self.j0_v = j0_v
         self.Iu = slice(i0_u, i1_u)
@@ -133,71 +138,70 @@ class SGrid(object):
         self.X = np.arange(self.i0, self.i1)
         self.Y = np.arange(self.j0, self.j1)
         # Grid cell boundaries = psi-points
-        self.Xb = np.arange(self.i0-0.5, self.i1)
-        self.Yb = np.arange(self.j0-0.5, self.j1)
+        self.Xb = np.arange(self.i0 - 0.5, self.i1)
+        self.Yb = np.arange(self.j0 - 0.5, self.j1)
 
     # ------------------------------------
 
     def _init_vertical(self):
         """Init vertical structure"""
 
-        # Vinfo overrides Vfile overrides ncid
-        # if Vinfo:
-        #     use Vinfo
-        # elif Vfile:
-        #     use Vfile
-        # elif ncid has vertical structure
-        #     use ncid
-        # else
-        #     No vertical structure
+        # Vinfo overrides ncid
 
         self.vertical = True
 
-        if self._Vinfo:
+        if self._Vinfo:  # Explicitly given vertical info
             Vinfo = self._Vinfo
-            self.N = Vinfo['N']
-            self.hc = Vinfo['hc']
-            self.Vstretching = Vinfo.get('Vstretching', 1)
-            self.Vtransform = Vinfo.get('Vtransform', 1)
-            self.Cs_r = s_stretch(self.N, Vinfo['theta_s'], Vinfo['theta_b'],
-                                  stagger='rho', Vstretching=self.Vstretching)
-            self.Cs_w = s_stretch(self.N, Vinfo['theta_s'], Vinfo['theta_b'],
-                                  stagger='w', Vstretching=self.Vstretching)
+            self.N = Vinfo["N"]
+            self.hc = Vinfo["hc"]
+            self.Vstretching = Vinfo.get("Vstretching", 1)
+            self.Vtransform = Vinfo.get("Vtransform", 1)
+            self.Cs_r = s_stretch(
+                self.N,
+                Vinfo["theta_s"],
+                Vinfo["theta_b"],
+                stagger="rho",
+                Vstretching=self.Vstretching,
+            )
+            self.Cs_w = s_stretch(
+                self.N,
+                Vinfo["theta_s"],
+                Vinfo["theta_b"],
+                stagger="w",
+                Vstretching=self.Vstretching,
+            )
 
-        else:
+        else:  # Vertical info from the ROMS file
 
-            if self._Vfile:
-                f0 = Dataset(self._Vfile)  # separate file
-            else:
-                f0 = self.ncid       # use the file itself
+            f0 = self.ncid
 
             try:
-                self.hc = f0.variables['hc'].getValue()
+                self.hc = f0.variables["hc"].getValue()
+                self.Cs_r = f0.variables["Cs_r"][:]
+                self.Cs_w = f0.variables["Cs_w"][:]
             except KeyError:
                 # No vertical information, skip the rest
                 self.vertical = False
 
             if self.vertical:
-                self.Cs_r = f0.variables['Cs_r'][:]
-                self.Cs_w = f0.variables['Cs_w'][:]
+                self.Vstretching = f0.variables.get("Vstretching", None)
+                # Get S
+                try:
+                    self.s_rho = f0.variables["s_rho"][:]
+                    self.s_w = f0.variables["s_w"][:]
+                except KeyError:
+                    # No S information on file
+                    self.s_rho = None
+                    self.s_w = None
 
                 # Vertical grid size
                 self.N = len(self.Cs_r)
 
                 # Vertical transform
-                self.Vtransform = 1  # Default
-                # Look for standard_name attribute of variable s_rho
-                try:
-                    v = f0.variables['s_rho']
-                    if v.standard_name[-1] == '2':
-                        self.Vtransform = 2
-                # No variable s_rho or no standard_name attribute
-                except (KeyError, RuntimeError):
-                    pass              # keep old default Vtransform = 1
-
-            # Close separate file
-            if self._Vfile:
-                f0.close()
+                if "Vtransform" in f0.variables:
+                    self.Vtransform = f0.variables["Vtransform"].getValue()
+                else:
+                    self.Vtransform = 1
 
     # --------------
     # Lazy reading
@@ -211,24 +215,39 @@ class SGrid(object):
     # def field(self):
     #     return self.ncid.variables['field'][self.J, self.I]
 
-    for _field in ['h', 'mask_rho', 'lon_rho', 'lat_rho',
-                   'pm', 'pn', 'angle', 'f']:
-        exec("%s = lambda self: self.ncid.variables['%s'][self.J, self.I]"
-             % (_field, _field))
+    for _field in ["h", "mask_rho", "lon_rho", "lat_rho", "pm", "pn", "angle", "f"]:
+        exec(
+            "%s = lambda self: self.ncid.variables['%s'][self.J, self.I]"
+            % (_field, _field)
+        )
         exec("%s = _Lazy(%s)" % (_field, _field))
 
     # 3D depth structure
     @_Lazy
     def z_r(self):
         if self.vertical:
-            return sdepth(self.h, self.hc, self.Cs_r,
-                          stagger='rho', Vtransform=self.Vtransform)
+            return sdepth(
+                self.h,  # type: ignore
+                self.hc,
+                self.Cs_r,
+                self.s_rho,
+                stagger="rho",
+                Vtransform=self.Vtransform,
+                Vstretching=self.Vstretching,
+            )
 
     @_Lazy
     def z_w(self):
         if self.vertical:
-            return sdepth(self.h, self.hc, self.Cs_w,
-                          stagger='w', Vtransform=self.Vtransform)
+            return sdepth(
+                self.h,  # type: ignore
+                self.hc,
+                self.Cs_w,
+                self.s_w,
+                stagger="w",
+                Vtransform=self.Vtransform,
+                Vstretching=self.Vstretching,
+            )
 
     # ---------------------------------
     # Wrappers for romsutil functions
@@ -236,7 +255,9 @@ class SGrid(object):
 
     def zslice(self, F, z):
         if self.vertical:
-            return zslice(F, self.z_r, -abs(z))
+            return zslice(F, self.z_r, -abs(z))  # type: ignore
+        else:
+            return None
 
     def xy2ll(self, x, y):
         return (sample2D(self.lon_rho, x-self.i0, y-self.j0),
